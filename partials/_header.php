51e5421539c5f6b62370e206f0050538ae6df488
Drago78624 <maazahmed78624@gmail.com>
2022-08-11 19:16:41 +0500

    initial commit

---
+++ partials/_header.php
@@ -0,0 +1,136 @@
+<header class="header header-intro-clearance header-4">
+            <div class="header-top">
+                <div class="container">
+                    <div class="header-left">
+                        <a href="tel:#"><i class="icon-phone"></i>Call: +0123 456 789</a>
+                    </div><!-- End .header-left -->
+
+                    <!-- TO BE REMOVED WHEN LOG IN -->
+                    <div class="header-right">
+                        <ul class="top-menu">
+                            <li>
+                                <a href="#">Actions</a>
+                                <ul>
+                                    <li><a href="login.php">Login / Register</a></li>
+                                </ul>
+                            </li>
+                        </ul><!-- End .top-menu -->
+                    </div><!-- End .header-right -->
+
+                </div><!-- End .container -->
+            </div><!-- End .header-top -->
+
+            <div class="header-middle">
+                
+                <div class="container">
+                    <div class="header-left">
+                        <button class="mobile-menu-toggler">
+                            <span class="sr-only">Toggle mobile menu</span>
+                            <i class="icon-bars"></i>
+                        </button>
+                        
+                        <a href="index.html" class="logo">
+                            <img src="assets/images/demos/demo-4/logo.png" alt="Molla Logo" width="105" height="25">
+                        </a>
+                    </div><!-- End .header-left -->
+
+                    <div class="header-center">
+                        <div class="header-search header-search-extended header-search-visible d-none d-lg-block">
+                            <a href="#" class="search-toggle" role="button"><i class="icon-search"></i></a>
+                            <form action="#" method="get">
+                                <div class="header-search-wrapper search-wrapper-wide">
+                                    <label for="q" class="sr-only">Search</label>
+                                    <button class="btn btn-primary" type="submit"><i class="icon-search"></i></button>
+                                    <input type="search" class="form-control" name="q" id="q" placeholder="Search product ..." required>
+                                </div><!-- End .header-search-wrapper -->
+                            </form>
+                        </div><!-- End .header-search -->
+                    </div>
+
+                    <div class="header-right">
+
+                        <div class="dashboard wishlist">
+                            <a href="dashboard.html" title="Dashboard">
+                                <div class="icon">
+                                    <i class="icon-user"></i>
+                                </div>
+                                <p>Wishlist</p>
+                            </a>
+                        </div>
+
+                        <div class="wishlist">
+                            <a href="wishlist.html" title="Wishlist">
+                                <div class="icon">
+                                    <i class="icon-heart-o"></i>
+                                    <span class="wishlist-count badge">3</span>
+                                </div>
+                                <p>Wishlist</p>
+                            </a>
+                        </div>
+
+                        <div class="wishlist">
+                            <a href="wishlist.html" title="Cart">
+                                <div class="icon">
+                                    <i class="icon-shopping-cart"></i>
+                                    <span class="wishlist-count badge">3</span>
+                                </div>
+                                <p>Cart</p>
+                            </a>
+                        </div>
+                    </div><!-- End .header-right -->
+                </div><!-- End .container -->
+            </div><!-- End .header-middle -->
+
+            <div class="header-bottom sticky-header">
+                <div class="container">
+                    <div class="header-left">
+                        <div class="dropdown category-dropdown">
+                            <a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-display="static" title="Browse Categories">
+                                Browse Categories <i class="icon-angle-down"></i>
+                            </a>
+
+                            <div class="dropdown-menu">
+                                <nav class="side-nav">
+                                    <ul class="menu-vertical sf-arrows">
+                                        <li class="item-lead"><a href="#">Daily offers</a></li>
+                                        <li class="item-lead"><a href="#">Gift Ideas</a></li>
+                                        <li><a href="#">Beds</a></li>
+                                        <li><a href="#">Lighting</a></li>
+                                        <li><a href="#">Sofas & Sleeper sofas</a></li>
+                                        <li><a href="#">Storage</a></li>
+                                        <li><a href="#">Armchairs & Chaises</a></li>
+                                        <li><a href="#">Decoration </a></li>
+                                        <li><a href="#">Kitchen Cabinets</a></li>
+                                        <li><a href="#">Coffee & Tables</a></li>
+                                        <li><a href="#">Outdoor Furniture </a></li>
+                                    </ul><!-- End .menu-vertical -->
+                                </nav><!-- End .side-nav -->
+                            </div><!-- End .dropdown-menu -->
+                        </div><!-- End .category-dropdown -->
+                    </div><!-- End .header-left -->
+
+                    <div class="header-center">
+                        <nav class="main-nav">
+                            <ul class="menu sf-arrows">
+                                <li class="megamenu-container active">
+                                    <a href="index.php">Home</a>
+                                </li>
+                                <li>
+                                    <a href="category.php">Shop</a>
+                                </li>
+                                <li>
+                                    <a href="about.php">About</a>
+                                </li>
+                                <li>
+                                    <a href="contact.php">Contact us</a>
+                                </li>
+                            </ul><!-- End .menu -->
+                        </nav><!-- End .main-nav -->
+                    </div><!-- End .header-center -->
+
+                    <div class="header-right">
+                        <i class="la la-lightbulb-o"></i><p>Clearance<span class="highlight">&nbsp;Up to 30% Off</span></p>
+                    </div>
+                </div><!-- End .container -->
+            </div><!-- End .header-bottom -->
+        </header><!-- End .header -->
